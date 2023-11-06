c4095526958b99871d69dd9dd77779c4f2aa3c6e
Max Schweikart <hello@maxschweik.art>
2023-11-06 11:24:31 +0100

    code: separate RP and CRP gates in crotosolve

--- code/CrotosolveOptimizer.py
+++ code/CrotosolveOptimizer.py
@@ -4,40 +4,56 @@ import pennylane.numpy as np
 from numpy.typing import NDArray
 
 class CrotosolveOptimizer:
-    def step_and_cost(self, circuit, initial_params: NDArray[np.float_], updates_dataset=[], debug=False, full_output=False):
+    def step_and_cost(self, circuit, initial_rp_params: NDArray[np.float_], initial_crp_params: NDArray[np.float_], updates_dataset: list[float] = [], debug=False, full_output=False):
         """
         reconstruct and optimize the univariate cost functions independently
-
-        TODO: separate CRP params and RP params to save iterations
         """
 
-        prev = circuit(initial_params)
-        params = initial_params.copy()
+        prev = circuit(initial_rp_params, initial_crp_params)
         y_output = []
 
-        iterator = np.nditer(params, flags=['multi_index'])
-        for old_param_value in iterator:
-            param_index = iterator.multi_index
-            if debug: print(f"Optimizing parameter {param_index}...")
-            univariate = self.create_univariate(circuit, params, param_index)
-            reconstruction, constants = reconstruct(univariate)
-            new_param_value, new_fun_value = minimize_reconstruction(reconstruction, constants)
-            if debug: print(f"Parameter update for {param_index} from {old_param_value} to {new_param_value} ({new_fun_value})")
-            params[param_index] = new_param_value
-            updates_dataset.append(new_fun_value)
-            if full_output:
+        rp_params = initial_rp_params.copy()
+        crp_params = initial_crp_params.copy()
+
+        for params, gate in [(rp_params, "RP"), (crp_params, "CRP")]:
+            iterator = np.nditer(params, flags=['multi_index'])
+            for old_param_value in iterator:
+                param_index = iterator.multi_index
+                if debug: print(f"Optimizing {gate} parameter {param_index}...")
+
+                if gate == "RP":
+                    univariate = self._create_univariate_rp(circuit, rp_params, crp_params, param_index)
+                else:
+                    univariate = self._create_univariate_crp(circuit, rp_params, crp_params, param_index)
+                
+                # TODO: use more efficient reconstruction and minimization method for RP gates
+                reconstruction, constants = reconstruct(univariate)
+                new_param_value, new_fun_value = minimize_reconstruction(reconstruction, constants)
+
+                if debug: print(f"{gate} parameter update for {param_index} from {old_param_value} to {new_param_value} -> y = {new_fun_value}")
+                params[param_index] = new_param_value
+                updates_dataset.append(new_fun_value)
                 y_output.append(new_fun_value)
-        
+
         if full_output:
-            return params, prev, y_output
+            return (rp_params, crp_params), prev, y_output
         
-        return params, prev
+        return (rp_params, crp_params), prev
 
     @staticmethod
-    def create_univariate(circuit, params: NDArray[np.int_], param_index):
+    def _create_univariate_rp(circuit, rp_params: NDArray[np.int_], crp_params: NDArray[np.float_], param_index: tuple):
         def univariate(param_value):
-            updated_params = params.copy()
-            updated_params[param_index] = param_value
-            return circuit(updated_params)
+            updated_rp_params = rp_params.copy()
+            updated_rp_params[param_index] = param_value
+            return circuit(updated_rp_params, crp_params)
+        
+        return univariate
+
+    @staticmethod
+    def _create_univariate_crp(circuit, rp_params: NDArray[np.int_], crp_params: NDArray[np.float_], param_index: tuple):
+        def univariate(param_value):
+            updated_crp_params = crp_params.copy()
+            updated_crp_params[param_index] = param_value
+            return circuit(rp_params, updated_crp_params)
         
         return univariate
--- code/circuits.py
+++ code/circuits.py
@@ -6,17 +6,21 @@ def sim_03(num_layers: int = 5, num_qubits: int = 4):
     dev = qml.device("default.qubit", wires=num_qubits, shots=None)
 
     @qml.qnode(dev)
-    def circuit(params: NDArray[np.float_]):
+    def circuit(rp_params: NDArray[np.float_], crp_params: NDArray[np.float_]):
         """
-        params is a three-dimensional array where
-        params[l, q] are the three parameters for the q-th qubit in the l-th layer.
+        rp_params is a three-dimensional array (#layers, #qubits, 2) where
+        rp_params[l, q] are the RX and RZ parameters for the q-th qubit in the l-th layer.
+
+        crp_params is a two-dimensional array (#layers, #qubits - 1) where
+        crp_params[l, q] is the CRZ parameter for the q-th qubit in the l-th layer
         """
         for layer in range(num_layers):
             for qubit in range(num_qubits):
-                qml.RX(params[layer, qubit, 0], wires=qubit)
-                qml.RZ(params[layer, qubit, 1], wires=qubit)
+                qml.RX(rp_params[layer, qubit, 0], wires=qubit)
+                qml.RZ(rp_params[layer, qubit, 1], wires=qubit)
             for qubit in range(num_qubits - 2, -1, -1): # incl. start, excl. stop, step
-                qml.CRZ(params[layer, qubit, 2], (qubit + 1, qubit))
+                qml.CRZ(crp_params[layer, qubit], (qubit + 1, qubit))
+            qml.Barrier(only_visual=True)
         return qml.expval(qml.PauliZ(1))
 
     return circuit
