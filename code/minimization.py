e4b43d531190c6abb5898770f712772c0c513025
Max Schweikart <hello@maxschweik.art>
2023-11-28 15:00:58 +0100

    code: minimize rp reconstruction analytically

--- code/minimization.py
+++ code/minimization.py
@@ -2,7 +2,14 @@ import math
 from scipy.optimize import minimize
 import numpy as np
 
-def minimize_reconstruction(reconstruction, constants, debug = False):
+def minimize_rp_reconstruction(reconstruction, constants, debug = False):
+    if constants['d5'] > 0:
+        x = - constants['d4']
+    else:
+        x = - constants['d4'] + math.pi
+    return x, reconstruction(x)
+
+def minimize_crp_reconstruction(reconstruction, constants, debug = False):
     min_y1 = 2 * math.pi - 2 * constants["d2"]
     min_y2 = np.array([1, 3]) * math.pi - constants["d4"]
     closer_min_y2 = min_y2[np.argmin(np.abs(min_y2 - min_y1))]
@@ -12,3 +19,9 @@ def minimize_reconstruction(reconstruction, constants, debug = False):
 
     if debug: print(res)
     return res.x[0], res.fun
+
+def minimize_reconstruction(reconstruction, constants, debug = False, gate = "CRP"):
+    if gate == "RP":
+        return minimize_rp_reconstruction(reconstruction, constants, debug)
+    else:
+        return minimize_crp_reconstruction(reconstruction, constants, debug)
